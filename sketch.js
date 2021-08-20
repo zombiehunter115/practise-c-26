af36aeaa744a95bba3e43b5a6701be7e5a9f7413
zombiehunter115 <andrew.tabang724@gmail.com>
2021-08-20 18:44:28 -0500

    updated the code by adding a platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,7 +18,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(150,290,300,220)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -40,12 +41,14 @@ function setup(){
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
+    text (mouseX + "," + mouseY , mouseX , mouseY)  
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
+    platform.display()
     pig1.display();
     log1.display();
 
